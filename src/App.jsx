f67fa7d2b785716e7f3f91602511d1c881e25fb6
szatolokin <staszatolokin28@gmail.com>
2022-06-22 15:02:38 +0300

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import { Container } from "./ui/Container/Container";
 export const App = () => {
   return (
     <Container>
-      <h1>hello, world</h1>
-      <Button>123</Button>
-      <Button isActive>123</Button>
-      <Button>123</Button>
+      <h1>magnit-front</h1>
     </Container>
   );
 };

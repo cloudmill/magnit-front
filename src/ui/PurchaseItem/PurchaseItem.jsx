d55f80b9fec0493f87cde2632a2a1c2eb8153768
kirill matveev <dejar@protonmail.com>
2022-06-24 17:17:42 +0300

    worked on the adaptive of the main page

--- src/ui/PurchaseItem/PurchaseItem.jsx
+++ src/ui/PurchaseItem/PurchaseItem.jsx
@@ -11,10 +11,12 @@ export const PurchaseItem = () => (
       <div className={styles.txt}>Поставка контейнеров для мусора на колесах с педалью, 120 л, RAL 6020 (заказ) по потребности ТБ ГМ Магнит в 3 квартале 2022 года. Поставка контейнеров для мусора на колесах с педалью, 120 л, RAL 6020 (заказ) по потребности ТБ ГМ Магнит в 3 квартале 2022 года.</div>
     </div>
     <div className={styles.col}>
+      <div className={styles.mob_ttl}>Площадка приема заявок</div>
       <GeoMark />
       <div className={styles.txt}>ЭТП B2B-Center</div>
     </div>
     <div className={styles.col}>
+      <div className={styles.mob_ttl}>Прием заявок завершается</div>
       <ClockMark />
       <div className={styles.txt}>03.06.2022</div>
       <div className={styles.txt}>16:00 МСК</div>

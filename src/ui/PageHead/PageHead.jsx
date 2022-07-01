fea923ee81485cb340e16fd4e481e4c989a97796
kirill matveev <dejar@protonmail.com>
2022-07-01 12:15:02 +0300

    upd page head

--- src/ui/PageHead/PageHead.jsx
+++ src/ui/PageHead/PageHead.jsx
@@ -1,28 +1,20 @@
 import styles from "./PageHead.module.scss";
-import { ReactComponent as BcArrow } from "../../assets/images/breadcrumbs-arrow.svg";
+import { Breadcrumbs } from "../Breadcrumbs/Breadcrumbs";
 
 function PageHead(props) {
 
   if (props.bc3) {
     return <>
-      <div className={styles.breadcrumbs}>
-        <a href="/" className={styles.item} > { props.bc1 } </a>
-        <BcArrow />
-        <a href="/" className={styles.item}> { props.bc2 } </a>
-        <BcArrow />
-        <div className={styles.item}> { props.bc3 } </div>
-      </div>
+
+      <Breadcrumbs bc1={props.bc1} bc2={props.bc2} bc3={props.bc3} />
 
       <h1 className={styles.ttl}> { props.title } </h1>
     </>
   }
 
   return <>
-          <div className={styles.breadcrumbs}>
-            <a href="/" className={styles.item} > { props.bc1 } </a>
-            <BcArrow />
-            <div className={styles.item}> { props.bc2 } </div>
-          </div>
+
+          <Breadcrumbs bc1={props.bc1} bc2={props.bc2} />
 
           <h1 className={styles.ttl}> { props.title } </h1>
         </>

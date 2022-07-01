a3ec9a0960a0f270774d0e59057c998090deb61c
kirill matveev <dejar@protonmail.com>
2022-07-01 13:27:58 +0300

    worked on the forms states

--- src/ui/ContactsForm/ContactsForm.jsx
+++ src/ui/ContactsForm/ContactsForm.jsx
@@ -9,26 +9,55 @@ export const ContactsForm = () => (
     <div className={classNames(styles.ttl, styles.onlyMobile)}>Вы можете обратиться по вопросам сотрудничества с ПАО «Магнит», заполнив форму ниже</div>
 
     <form className={styles.form}>
-      <input className={styles.input} type='text' placeholder="Название организации*"/>
-      <input className={styles.input} type='number' placeholder="ИНН"/>
-      <input className={styles.input} type='email' placeholder="E-mail*"/>
-      <input className={styles.input} type='text' placeholder="ФИО*"/>
-      <input className={classNames(styles.input, styles.fullWidth)} type='tel' placeholder="Телефон"/>
+
+      <label className={styles.label}>
+        <input className={styles.input} type='text' placeholder="Название организации*"/>
+      </label>
+      
+      <label className={styles.label}>
+        <input className={styles.input} type='number' placeholder="ИНН"/>
+      </label>
+
+      <label className={styles.label}>
+        <input className={styles.input} type='email' placeholder="E-mail*"/>
+      </label>
+
+      <label className={styles.label}>
+        <input className={styles.input} type='text' placeholder="ФИО*"/>
+      </label>
+
+      <label className={classNames(styles.label, styles.fullWidth)}>
+        <input className={classNames(styles.input, styles.fullWidth, styles.error)} type='tel' placeholder="Телефон" value='7900'/>
+        <div className={classNames(styles.inputError)}>Error text</div>
+      </label>
+      
       <textarea className={styles.textarea} placeholder="Текст обращения*"/>
 
       <div className={styles.bottom}>
+
         <div className={styles.col}>
           <GreyButton type="file"><Clip/> Прикерпить файл</GreyButton>
           <div className={classNames(styles.note, styles.onlyDesktop)}>Допустимое расширение файлов: .ppt, .pptx, .doc, .docx, .xls, <br/> .xlsx, .pdf, .jpg, .jpeg, .png</div>
           <div className={classNames(styles.note, styles.onlyMobile)}>Допустимое расширение файлов: .ppt, .pptx, .doc, .docx, .xls, .xlsx, .pdf, .jpg, .jpeg, .png</div>
         </div>
+
         <div className={styles.col}>
           <div className={styles.checkbox}>
-            <input className={styles.box} type="checkbox"/>
+            <label className={styles.boxLabel}>
+              <input type="checkbox"/>
+
+              <div className={styles.box}>
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M15 1L5 1C3.11438 1 2.17157 1 1.58579 1.58579C1 2.17157 1 3.11438 1 5V15C1 16.8856 1 17.8284 1.58579 18.4142C2.17157 19 3.11438 19 5 19H15C16.8856 19 17.8284 19 18.4142 18.4142C19 17.8284 19 16.8856 19 15V5C19 3.11438 19 2.17157 18.4142 1.58579C17.8284 1 16.8856 1 15 1Z" fill="#ED0E00" stroke="#ED0E00" stroke-width="2"/>
+                  <path d="M6 10L9 13L14 7.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+              </div>
+            </label>
             <div className={styles.txt}>Согласен с условиями обработки <a href="/">персональных данных</a></div>
           </div>
           <RedButton type="submit">Отправить</RedButton>
         </div>
+
       </div>
     </form>
   </div>

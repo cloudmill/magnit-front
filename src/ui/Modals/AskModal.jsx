979fbdf969d0b8d54be3249226f1315c2f9b912b
kirill matveev <dejar@protonmail.com>
2022-07-08 11:07:28 +0300

    update modal

--- src/ui/Modals/AskModal.jsx
+++ src/ui/Modals/AskModal.jsx
@@ -1,12 +1,20 @@
 import styles from "./Modals.module.scss";
 import classNames from "classnames";
 import { PurchaseForm } from "../PurchaseForm/PurchaseForm";
+import { useDispatch } from "react-redux";
+import { close } from "../../store/index.ts";
 
 function AskModal(props) {
+  const dispatch = useDispatch();
 
   return <div className={classNames(styles.askModal)}>
 
     <PurchaseForm inModal="true" ttl="Задать вопрос"/>
+
+    <button className={styles.close} onClick={() => {
+      dispatch(close());
+      document.querySelector('body').style.overflow = '';
+    }}></button>
 
   </div>
   
